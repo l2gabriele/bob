3609367463da7f0609697e9e2a49ef6eaf190f61
Laura Gabriele <l2gabriele@gmail.com>
2018-02-28 10:20:54 -0700

    aking sure its commited

--- src/index.js
+++ src/index.js
@@ -4,34 +4,18 @@ bar(); //webpack
 
 console.log("yay it worked");
 
-
 function openNav() {
-  console.log("button click")
+  console.log("open button click")
   document.getElementById("navBar").style.width="20%";
   document.getElementById("main").style.marginLeft="20%";
 };
 document.getElementById("open-btn").addEventListener('click', openNav);
 
 function closeNav() {
-  console.log("button click")
+  console.log("close button click")
   document.getElementById("navBar").style.width="0";
   document.getElementById("main").style.marginLeft="0";
 };
 document.getElementById("close-btn").addEventListener('click', closeNav);
 
-$(document).ready(function() {
-  $(".open-btn").click(function() {
-    $("#navBarLink").fadeIn("slow");
-  });
-/*$(document).click(function() {
-   $("button").hide();
-   $("button").each(function(i) {
-       if (this.complete) {
-           $(this).fadeIn(3000);
-       } else {
-           $(this).load(function() {
-               $(this).fadeIn(3000);
-           });
-       }
-   });*/
 });
